a05c10b365eaf4d3fd63101c97201615548486a6
sea <sea11rod@gmail.com>
2023-06-16 11:53:33 +0530

    search done

--- food/urls.py
+++ food/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, Menu
+from .views import index, Menu, about
 
 
 urlpatterns = [
     path("", index, name="home"),
     path("menu/", Menu.as_view(), name="menu"),
+    path("about/", about, name="about"),
 ]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
+from django.http import HttpRequest, HttpResponse
 from django.views.generic import ListView
 from django.shortcuts import render
-from .models import FoodCategory
+from .models import FoodCategory, Food
 
 
 def index(request):
@@ -11,3 +13,28 @@ class Menu(ListView):
     model = FoodCategory
     template_name = "menu.html"
     context_object_name = "food_category"
+    obj = None
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        search = request.GET.get("search", None)
+        print(search)
+        if search:
+            self.obj = Food.objects.filter(name__contains=search)
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        if self.obj is not None:
+            context = super().get_context_data(**kwargs)
+            context[self.context_object_name] = None
+            context["food_search"] = self.obj
+            context["search_head"] = "No search Result"
+            if self.obj:
+                context["search_head"] = "Search Results"
+            return context
+
+        return super().get_context_data(**kwargs)
+
+
+def about(request):
+    return render(request, "about.html")
